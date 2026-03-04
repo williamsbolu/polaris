04b06bf1fd02a28a1a0565b94ea28ec3707d5005
williams bolu <williams.bolu99@gmail.com>
2026-03-04 19:37:41 +0100

    03: database setup

---
+++ src/features/auth/components/unauthenticated-view.tsx
@@ -0,0 +1,39 @@
+import { ShieldAlert } from "lucide-react";
+
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+
+export const UnauthenticatedView = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-background">
+      <div className="w-full max-w-lg bg-muted">
+        <Item variant="outline">
+          <ItemMedia variant="icon">
+            <ShieldAlert />
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>Unauthorized Access</ItemTitle>
+            <ItemDescription>
+              You are not authorized to access this resource.
+            </ItemDescription>
+          </ItemContent>
+          <ItemActions>
+            <SignInButton>
+              <Button variant="outline" size="sm">
+                Sign in
+              </Button>
+            </SignInButton>
+          </ItemActions>
+        </Item>
+      </div>
+    </div>
+  );
+};
