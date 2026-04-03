21ceddc34f73da764c4e68bf53dceee031058602
Williams Bolu <williams.bolu99@gmail.com>
2026-04-03 13:26:05 +0100

    04: firecrawl ai

--- src/app/api/demo/blocking/route.ts
+++ src/app/api/demo/blocking/route.ts
@@ -1,9 +1,10 @@
 import { google } from "@ai-sdk/google";
-import { generateText, } from "ai";
+import { generateText } from "ai";
 
 export async function POST() {
   const response = await generateText({
     model: google("gemini-2.5-flash"),
+    // model: anthropic("claude-3-5-sonnet-20240620"),  // any model from the ai sdk can be used here
     prompt: "Write a vegetarian lasagna recipe for 4 people.",
   });
 
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,41 @@
 import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
 import { inngest } from "./client";
+import { firecrawl } from "@/lib/firecrawl";
+
+const URL_REGEX = /https?:\/\/[^\s]+/g;
 
 export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "demo/generate" },
-  async ({ step }) => {
+  async ({ event, step }) => {
+    const { prompt } = event.data as { prompt: string };
+
+    const urls = (await step.run("extract-urls", async () => {
+      return prompt.match(URL_REGEX) || [];
+    })) as string[];
+
+    const scrapedContent = await step.run("scrape-urls", async () => {
+      const results = await Promise.all(
+        urls.map(async (url) => {
+          const result = await firecrawl.scrape(url, { formats: ["markdown"] });
+          return result.markdown || null;
+        }),
+      );
+
+      // filter out any of the null unsuccessfull scrapes
+      return results.filter(Boolean).join("\n\n");
+    });
+
+    const finalPrompt = scrapedContent
+      ? `Content:\n${scrapedContent}\n\nQuestion: ${prompt}`
+      : prompt; // if no content is scraped, use the original prompt
+
     await step.run("generate-text", async () => {
       return await generateText({
         model: google("gemini-2.5-flash"),
-        prompt: "Write a vegetarian lasagna recipe for 4 people.",
+        // model: anthropic("claude-3-5-sonnet-20240620"),  // any model from the ai sdk can be used here
+        prompt: finalPrompt,
       });
     });
   },
